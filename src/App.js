1751b6c0e27fce83a77a53b46017c6f1e3a1f520
Clinton Brito <clintoncsb@gmail.com>
2023-04-05 21:19:49 -0300

    Resolvendo requisitos 3 e 4, porém estão bugados ainda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: '',
+    hasTrunfo: false,
+  }
+
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
---
+++ src/components/Card.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <>
+        <h2 data-testid="name-card">{ cardName }</h2>
+        <img
+          data-testid="image-card"
+          src={ cardImage }
+          alt={ cardName }
+        />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <h3 data-testid="attr1-card">{ cardAttr1 }</h3>
+        <h3 data-testid="attr2-card">{ cardAttr2 }</h3>
+        <h3 data-testid="attr3-card">{ cardAttr3 }</h3>
+        <h3 data-testid="rare-card">{ cardRare }</h3>
+        <h2 data-testid="trunfo-card">Super Trunfo</h2>
+      </>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardName: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+}.isRequired;
